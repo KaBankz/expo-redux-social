412217a0459c59cb7fe2613e9d3671033ab448e6
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-05-17 19:25:14 -0400

    Create async post thunk to fetch data from api

--- state/posts/postSlice.ts
+++ state/posts/postSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 interface Post {
   userId: number;
@@ -7,12 +7,37 @@ interface Post {
   body: string;
 }
 
-const initialState: Post[] = [];
+const initialState = {
+  data: [] as Post[],
+  loading: false,
+  error: null,
+};
+
+// async thunk for fetching data
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data: Post[] = await response.json();
+  return data;
+});
 
 export const postSlice = createSlice({
-  name: 'post',
+  name: 'posts',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchPosts.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    }),
+      builder.addCase(fetchPosts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      }),
+      builder.addCase(fetchPosts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default postSlice.reducer;
