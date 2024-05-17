47057cfd117337bb2afecf26cded05903d8c46d4
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-05-17 18:19:37 -0400

    Create basic feed

--- App.tsx
+++ App.tsx
@@ -1,18 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Post from './components/Post';
 
 export default function App() {
+  const data = Array.from({ length: 10 });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.body}>
+        <Text style={styles.title}>Feed</Text>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Post />}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  body: {
+    height: '100%',
+    backgroundColor: '#333',
+    padding: 10,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 48,
+    fontWeight: 'bold',
   },
 });
