17ba9fcb064ecb544125dce2f03ff60209a850dc
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-05-17 20:12:30 -0400

    Add comments to ui
    Yes, this is a bad implementation, loading all the
    existing comments for each existing post is VERY
    bad, might as well be a DDOS. But good for now.
    
    Future refactor will only load the comments for
    the specific post, instead of all of them.

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,13 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { useSelector } from 'react-redux';
+import { RootState } from '../state/store';
 
-export default function Comment() {
+export default function Comment(props: { id: number }) {
+  const comment = useSelector((state: RootState) =>
+    state.comments.data.find((c) => c.id === props.id)
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.username}>User</Text>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-        reprehenderit!
-      </Text>
+      <Text style={styles.username}>{comment.email}</Text>
+      <Text>{comment.body}</Text>
     </View>
   );
 }
--- components/Post.tsx
+++ components/Post.tsx
@@ -1,16 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { useSelector } from 'react-redux';
-import { RootState } from '../state/store';
+import { useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchComments } from '../state/comments/commentSlice';
+import { AppDispatch, RootState } from '../state/store';
+import Comment from './Comment';
 
 export default function Post(props: { id: number }) {
   const post = useSelector((state: RootState) =>
     state.posts.data.find((c) => c.id === props.id)
   );
 
+  const dispatch = useDispatch<AppDispatch>();
+  const { data, error, loading } = useSelector(
+    (state: RootState) => state.comments
+  );
+
+  const comments = data.filter((c) => c.postId === props.id);
+
+  useEffect(() => {
+    dispatch(fetchComments());
+  }, [dispatch]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.postTitle}>{post.title}</Text>
       <Text>{post.body}</Text>
+      <Text style={styles.commentTitle}>Comments:</Text>
+      {loading && <ActivityIndicator size='small' />}
+      {error && <Text style={{ color: 'red' }}>{error}</Text>}
+      {!loading &&
+        !error &&
+        comments.map((comment) => <Comment id={comment.id} />)}
     </View>
   );
 }
@@ -25,5 +45,10 @@ const styles = StyleSheet.create({
   postTitle: {
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  commentTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 10,
   },
 });
